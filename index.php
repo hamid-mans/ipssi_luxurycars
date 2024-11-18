e3346c458da618a4dc57a32d88d2bb08827b9012
hamid-mans <h.mansouri2310@gmail.com>
2024-11-18 17:21:56 +0100

    Implémenter le menu et le design du menu

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
     <body>
         <header>
             <p class="title">Luxury Cars</p>
+
+	        <nav>
+		        <ul>
+			        <li><a href="#" class="ui teal button">Créer mon compte</a></li>
+			        <li><a href="#" class="ui button">Me connecter</a></li>
+
+			        <li><a href="#" class="ui button">Véhicules</a></li>
+		        </ul>
+	        </nav>
         </header>
     </body>
 </html>
